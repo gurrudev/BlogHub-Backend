5f5e46b5f7e1ca13a0265db631eabf0d6a5d6ae1
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-24 15:49:45 +0530

    cors updated

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const userRouter = require('./router/users.routes')
 
 const app = express()
 
-const corsOptions = {
-  origin: ['https://bloghubsite.netlify.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
+// const corsOptions = {
+//   origin: ['https://bloghubsite.netlify.app', 'http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json())
 dbConnect()
@@ -20,12 +20,12 @@ dbConnect()
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
 
-app.options('/api/users/user-data', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).end(); // End the preflight request
-});
+// app.options('/api/users/user-data', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.status(200).end(); // End the preflight request
+// });
 
 app.get('/', (req, res) => {
     res.status(200).send('App is live!🚀')

4806726f75be9217bb2db10a26900681be29a11f
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-24 13:37:59 +0530

    cors url

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ dbConnect()
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
 
+app.options('/api/users/user-data', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(200).end(); // End the preflight request
+});
+
 app.get('/', (req, res) => {
     res.status(200).send('App is live!🚀')
 })

e19cad20fe49e8ffc1c4c74c05094279c8abcf98
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-24 13:28:14 +0530

    cors option updated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -131,6 +131,11 @@ class UserController {
     };
 
     static getUserData = async (req, res, next) => {
+
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         const token = req.headers.authorization;
 
         if (!token) {

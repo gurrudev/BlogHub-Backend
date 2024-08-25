65ab0f0c07d6e4ae4cef21154b5b5887efa84697
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-25 17:45:14 +0530

    swagger added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors')
 const blogsRouter = require('./router/blogs.routes')
 const dbConnect = require('./config/dbConfig')
 const userRouter = require('./router/users.routes')
+const YAML = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
+
 
 const app = express()
 
@@ -10,6 +13,10 @@ app.use(cors());
 
 app.use(express.json())
 dbConnect()
+
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app')
 const PORT = 3002
 
 app.listen(PORT, (req, res) =>{
-    console.log(`server is running ${PORT}`);
+    console.log(`server is running http://127.0.0.1:${PORT}`);
 })

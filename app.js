829822d5733a06b680df6490ec01d59f9f143c97
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-25 18:47:43 +0530

    swagger removed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 dbConnect()
 
 // swagger route
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // api routes
 app.use('/api/blogs', blogsRouter)

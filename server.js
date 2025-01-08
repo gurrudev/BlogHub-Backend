f43544c983e35555e30ca3e0b7f9642c86fff61b
gurrudev <pawarash000@outlook.com>
2025-01-08 13:36:42 +0530

    UPDATE: Clusters Created

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const app = require('./app')
+const cluster = require('cluster')
+const os = require('os')
 
+const numCPUs = os.cpus().length
 
-const PORT = 3002
-
-app.listen(PORT, (req, res) =>{
-    console.log(`server is running http://127.0.0.1:${PORT}`);
-})
+if (cluster.isMaster) { 
+    console.log(`Master ${process.pid} is running`)
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`)
+    })
+} else {
+    console.log(`Worker ${process.pid} started`)
+    const PORT = 3002
+    app.listen(PORT, (req, res) =>{
+        console.log(`Server is running http://127.0.0.1:${PORT}`);
+    })
+}
